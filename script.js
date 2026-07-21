dcdb61bc578c0552b8f855c0412e1a8d1c0d0541
juancruzastra <133728110+juancruzastra@users.noreply.github.com>
2026-07-21 13:44:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "PEGAR_URL_DE_SUPABASE";
-const SUPABASE_ANON_KEY = "PEGAR_PUBLISHABLE_KEY_DE_SUPABASE";
+const SUPABASE_URL = "https://miiskclwrvbmgqkdswro.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_PkTLIcz-j9wyXkuTjp_yvg_t8coIacO";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
